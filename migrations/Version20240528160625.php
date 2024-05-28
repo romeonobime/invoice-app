6da14b30bc613a09e94d778b6b4b3e65e207c82a
Romeo Nobime <romeo.nobime@protonmail.com>
2024-05-28 18:52:14 +0200

    feature: add the new entities address, client, invoice, invoiceStatus, item, paymentTerms to the database

---
+++ migrations/Version20240528160625.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240528160625 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE invoice DROP INDEX UNIQ_90651744A133F1B8, ADD INDEX IDX_90651744A133F1B8 (sender_address_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE invoice DROP INDEX IDX_90651744A133F1B8, ADD UNIQUE INDEX UNIQ_90651744A133F1B8 (sender_address_id)');
+    }
+}
